2864919173f5ab04a96b19ec680844627cb9d5c1
Nathan Friend <n.friend@cant-col.ac.uk>
2013-02-12 09:16:40 +0000

    Added hub MVC. Added .htaccess file to remove index.php from URL. Updated routes file, making hub default view.

---
+++ application/views/hub/view.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>MyCPD Hub</title>
+        <link href="http://webdev-04.cant-col.ac.uk/MyCPD//assets/css/default.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    </head>
+    <body>
+        <h1>MyCPD Hub</h1>
+    <li><a href="http://webdev-04.cant-col.ac.uk/MyCPD/learning_plan/view">Learning Plan</a></li>
+    <li><a href="http://webdev-04.cant-col.ac.uk/MyCPD/target/view">Targets</a></li>
+    </body>
+</html>
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
