ee1e76416b2fd0225b2f8b962efd6318d98f9308
Nathan Friend <n.friend@cant-col.ac.uk>
2013-02-15 11:21:41 +0000

    Added v_targets_with_status view.

--- _db_schema/mycpd.sql
+++ _db_schema/mycpd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 08, 2013 at 12:11 PM
+-- Generation Time: Feb 15, 2013 at 11:20 AM
 -- Server version: 5.1.66-community
 -- PHP Version: 5.3.10
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `mycpd`
 --
+CREATE DATABASE `mycpd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mycpd`;
 
 -- --------------------------------------------------------
 
@@ -69,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `Learning_plan_detail` (
   KEY `learning_plan_id` (`learning_plan_id`),
   KEY `learning_plan_target_id` (`learning_plan_target_id`),
   KEY `priority_type_id` (`priority_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 -- --------------------------------------------------------
 
@@ -83,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `Learning_plan_target` (
   `description` varchar(250) NOT NULL,
   `sort_order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -101,19 +103,12 @@ CREATE TABLE IF NOT EXISTS `Priority_type` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `targets`
+-- Table structure for table `news`
 --
 
-CREATE TABLE IF NOT EXISTS `targets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) DEFAULT NULL,
-  `description` varchar(600) DEFAULT NULL,
-  `status_id` int(11) DEFAULT NULL,
-  `user_id` int(20) DEFAULT NULL,
-  `target_date` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `targets_ibfk_2` (`status_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+CREATE TABLE IF NOT EXISTS `news` (
+  `description` varchar(500) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -127,6 +122,24 @@ CREATE TABLE IF NOT EXISTS `target_status` (
   `sort_order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `targets`
+--
+
+CREATE TABLE IF NOT EXISTS `targets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) DEFAULT NULL,
+  `description` varchar(600) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `employee_id` int(20) DEFAULT NULL,
+  `target_date` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `targets_ibfk_2` (`status_id`),
+  KEY `targets_ibfk_1` (`employee_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=29 ;
 
 -- --------------------------------------------------------
 
@@ -149,11 +162,33 @@ CREATE TABLE IF NOT EXISTS `v_learning_plan_detail` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `v_targets_with_status`
+--
+CREATE TABLE IF NOT EXISTS `v_targets_with_status` (
+`id` int(11)
+,`title` varchar(150)
+,`description` varchar(600)
+,`status` varchar(50)
+,`employee_id` int(20)
+,`target_date` varchar(20)
+);
+-- --------------------------------------------------------
+
+--
 -- Structure for view `v_learning_plan_detail`
 --
 DROP TABLE IF EXISTS `v_learning_plan_detail`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_learning_plan_detail` AS (select `lpd`.`learning_plan_id` AS `learning_plan_id`,`lpd`.`id` AS `learning_plan_detail_id`,`lpd`.`title` AS `title`,`lpd`.`learning_outcomes` AS `learning_outcomes`,`lpt`.`description` AS `target_description`,`pt`.`description` AS `priority_type`,`lpd`.`target_date` AS `target_date`,(case when (`lpd`.`is_completed` = 1) then 'Y' else 'N' end) AS `is_completed`,`lpd`.`evaluation_url` AS `evaluation_url`,`lpt`.`sort_order` AS `target_sort_order`,`pt`.`sort_order` AS `priority_sort_order` from ((`Learning_plan_detail` `lpd` left join `Learning_plan_target` `lpt` on((`lpd`.`learning_plan_target_id` = `lpt`.`id`))) left join `Priority_type` `pt` on((`lpd`.`priority_type_id` = `pt`.`id`))));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `v_targets_with_status`
+--
+DROP TABLE IF EXISTS `v_targets_with_status`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`mycpd_admin`@`%` SQL SECURITY DEFINER VIEW `v_targets_with_status` AS select `targets`.`id` AS `id`,`targets`.`title` AS `title`,`targets`.`description` AS `description`,`target_status`.`title` AS `status`,`targets`.`employee_id` AS `employee_id`,`targets`.`target_date` AS `target_date` from (`targets` join `target_status` on((`targets`.`status_id` = `target_status`.`id`)));
 
 --
 -- Constraints for dumped tables
@@ -177,6 +212,7 @@ ALTER TABLE `Learning_plan_detail`
 -- Constraints for table `targets`
 --
 ALTER TABLE `targets`
+  ADD CONSTRAINT `targets_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `Employee` (`id`),
   ADD CONSTRAINT `targets_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `target_status` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
