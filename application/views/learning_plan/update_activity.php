53e5ae3fed539bedb4299b88f31438e997509ba1
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-02-27 12:44:44 -0430

    fix format

--- application/views/learning_plan/update_activity.php
+++ application/views/learning_plan/update_activity.php
@@ -25,10 +25,10 @@
                    value="<?= $activity->planned_date ?>" /><br />
             
             <label for="title">Title of activity/event</label> 
-            <textarea name="title"><?= $activity->title ?></textarea><br />
+            <textarea name="title" rows="4" cols="50"><?= $activity->title ?></textarea><br />
 
             <label for="learning_outcomes">Intended learning outcomes</label>
-            <textarea name="learning_outcomes"><?= $activity->learning_outcomes ?></textarea><br />
+            <textarea name="learning_outcomes" rows="4" cols="50"><?= $activity->learning_outcomes ?></textarea><br />
             
             <label for="cpd_type_id">CPD type</label>
             <?= form_dropdown('cpd_type_id', $cpd_types, $activity->cpd_type_id) ?><br />
