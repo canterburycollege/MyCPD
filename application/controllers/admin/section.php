f139bea7900eb303ef267f14ec892184dcd89fba
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-03-08 10:12:25 -0300

    add section & faculty controllers

---
+++ application/controllers/admin/section.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Controller for Section maintenance
+ * 
+ * Loads models and views required for admin functions 
+ *
+ * @author rh
+ */
+class Faculty extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url_helper');
+    }
+    
+    public function create(){
+        /**
+         * @todo add code
+         */
+    }
+    
+    public function delete($id){
+        /**
+         * @todo code
+         */
+    }
+    
+    public function index(){
+        $this->load->view('admin/faculty');
+    }
+    
+    public function update($id){
+        /**
+         * @todo add code
+         */
+    }
+}
+
+?>
