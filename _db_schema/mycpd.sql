205c9fd1d380368f7758aeed925768675d68eb4f
Nathan Friend <n.friend@cant-col.ac.uk>
2013-03-22 09:31:13 +0000

    Updated DB script to include new fields.

--- _db_schema/mycpd.sql
+++ _db_schema/mycpd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.5.6
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 15, 2013 at 04:08 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.4.3
+-- Generation Time: Mar 22, 2013 at 09:17 AM
+-- Server version: 5.1.66-community-log
+-- PHP Version: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `activity`
 --
 
-DROP TABLE IF EXISTS `activity`;
 CREATE TABLE IF NOT EXISTS `activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `employee_id` int(11) NOT NULL,
@@ -36,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `activity` (
   `planned_date` date NOT NULL,
   `cpd_type_id` int(11) DEFAULT NULL,
   `target_id` int(11) NOT NULL,
+  `impact` varchar(250) DEFAULT NULL,
   `priority_type_id` int(11) DEFAULT NULL,
   `completed_date` date DEFAULT NULL,
   `evaluation_url` varchar(50) DEFAULT NULL,
@@ -46,16 +46,19 @@ CREATE TABLE IF NOT EXISTS `activity` (
   KEY `priority_type_id` (`priority_type_id`),
   KEY `employee_id` (`employee_id`),
   KEY `cpd_type_id` (`cpd_type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `activity`
 --
 
-INSERT INTO `activity` (`id`, `employee_id`, `title`, `provider`, `learning_outcomes`, `planned_date`, `cpd_type_id`, `target_id`, `priority_type_id`, `completed_date`, `evaluation_url`, `hours_of_cpd`, `rating`) VALUES
-(1, 1, '                                                                                                                                                                Assessing learning                                                                        ', '', '                                                                                                                                                      Gather a range of assesment techniques to implement in class                                        ', '2013-02-26', 1, 1, 7, '2013-02-26', ' ', '0.00', 0),
-(2, 1, 'Mentoring new colleagues                      ', '', 'Some more learning outcomes, this field is currently set to have a maximum of 600 characters available in  the database.', '2013-02-26', 1, 2, 7, '2013-02-19', ' ', '0.00', 0),
-(3, 1, 'Parents evening', '', 'some really important learning outcomes            ', '2013-03-01', 1, 4, 8, '0000-00-00', '', '0.00', 0);
+INSERT INTO `activity` (`id`, `employee_id`, `title`, `provider`, `learning_outcomes`, `planned_date`, `cpd_type_id`, `target_id`, `impact`, `priority_type_id`, `completed_date`, `evaluation_url`, `hours_of_cpd`, `rating`) VALUES
+(1, 1, 'Rise and Shine', '', 'Creative assessment techniques to implement in to practice', '2013-02-26', 1, 1, 'Lessons will be structured with regular assessment activities so students are on task and know their progress rate', 7, '2013-03-12', ' ', '0.00', 0),
+(2, 1, 'Rise and Shine            ', '', 'Effective questioning technique            ', '2013-02-26', 1, 2, 'Questions will encourage students to think independently, keep them engaged and included', 8, '2013-03-22', ' ', '0.00', 0),
+(3, 1, ' Parents evening            ', '', 'Some really important learning outcomes                        ', '2013-03-01', 1, 4, 'Review dyslexia strategies', 8, '2013-03-19', '', '0.00', 0),
+(4, 1, 'Claro read training', '', 'Understand the uses of Claro read, how to integrate into lessons and basic awareness of features', '2013-04-19', 1, 4, 'Integration of learning support technology for dyslexic students', 7, NULL, NULL, '0.00', NULL),
+(5, 1, 'Peer Observation', '', 'See Colleague C teaching level 1 class using Interactive whiteboard', '2013-03-27', 1, 1, 'Transfer good practice to my level 1 session next Wednesday as part of group assessment task', 8, NULL, NULL, '0.00', NULL),
+(6, 1, 'Visit Stakeholder X', '', 'Organise enrichment activity for level 3 following meeting with Colleagues A and B', '2013-06-01', 1, 2, 'Experience a day in the life at Stakeholder X, gain employability skills and experience', 8, NULL, NULL, '0.00', NULL);
 
 -- --------------------------------------------------------
 
@@ -63,7 +66,6 @@ INSERT INTO `activity` (`id`, `employee_id`, `title`, `provider`, `learning_outc
 -- Table structure for table `cpd_type`
 --
 
-DROP TABLE IF EXISTS `cpd_type`;
 CREATE TABLE IF NOT EXISTS `cpd_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(50) NOT NULL,
@@ -87,7 +89,6 @@ INSERT INTO `cpd_type` (`id`, `description`, `sort_order`) VALUES
 -- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employee`;
 CREATE TABLE IF NOT EXISTS `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `display_name` varchar(50) NOT NULL,
@@ -106,53 +107,19 @@ INSERT INTO `employee` (`id`, `display_name`, `moodle_user_id`, `mycpd_access_gr
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee_section`
---
-
-DROP TABLE IF EXISTS `employee_section`;
-CREATE TABLE IF NOT EXISTS `employee_section` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_id` int(11) NOT NULL,
-  `section_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `employee_id` (`employee_id`,`section_id`),
-  KEY `section_id` (`section_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `faculty`
---
-
-DROP TABLE IF EXISTS `faculty`;
-CREATE TABLE IF NOT EXISTS `faculty` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
-  `manager` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `faculty`
---
-
-INSERT INTO `faculty` (`id`, `title`, `manager`) VALUES
-(1, 'Arts, Media & Publishing', 'Mark Howland'),
-(2, 'Engineering & Construction', 'faculty head1'),
-(3, 'Enterprise & Partnerships', 'Faculty Head2'),
-(5, 'Higher Education', 'Faculty Head3');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `news`
 --
 
-DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
-  `description` varchar(500) CHARACTER SET latin1 DEFAULT NULL
+  `description` varchar(1024) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`description`) VALUES
+('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html>\n<head>\n</head>\n<body>\n<p>A new mandatory online learning module is now available for all staff to complete from DisabledGo. These modules look at equality in both legal and practical terms as well as language, types of impairment and advice on accessible services. <a href="http://training.disabledgo.com/auth/register/canterbury-college" target="_blank">http://training.disabledgo.com/auth/register/canterbury-college</a> Please follow the link and login by registering an email, username and password. You''ll also be able to access this link via the VLE shortly by looking under Staff Training.</p>\n</body>\n</html>');
 
 -- --------------------------------------------------------
 
@@ -160,7 +127,6 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Table structure for table `priority_type`
 --
 
-DROP TABLE IF EXISTS `priority_type`;
 CREATE TABLE IF NOT EXISTS `priority_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(50) NOT NULL,
@@ -180,37 +146,13 @@ INSERT INTO `priority_type` (`id`, `description`, `sort_order`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `section`
---
-
-DROP TABLE IF EXISTS `section`;
-CREATE TABLE IF NOT EXISTS `section` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL,
-  `manager` varchar(50) NOT NULL,
-  `faculty_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `faculty_id` (`faculty_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `section`
---
-
-INSERT INTO `section` (`id`, `title`, `manager`, `faculty_id`) VALUES
-(2, 'Arts & Media', 'Section Manager1', 1),
-(4, 'Music', 'Section Manager2', 1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `target`
 --
 
-DROP TABLE IF EXISTS `target`;
 CREATE TABLE IF NOT EXISTS `target` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(150) DEFAULT NULL,
+  `title_ext` varchar(150) DEFAULT NULL,
   `description` varchar(600) DEFAULT NULL,
   `status_id` int(11) DEFAULT NULL,
   `employee_id` int(20) DEFAULT NULL,
@@ -218,20 +160,16 @@ CREATE TABLE IF NOT EXISTS `target` (
   PRIMARY KEY (`id`),
   KEY `targets_ibfk_2` (`status_id`),
   KEY `targets_ibfk_1` (`employee_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `target`
 --
 
-INSERT INTO `target` (`id`, `title`, `description`, `status_id`, `employee_id`, `target_date`) VALUES
-(1, 'Timely assessment of students', 'This is the description for target 1', 7, 1, '28/02/2013'),
-(2, 'Update teaching & learning skills', 'A more detailed description of this target', 7, 1, NULL),
-(3, 'Update subject specialism', 'Some text to describe this target...', 7, 1, NULL),
-(4, 'Fulfil wider professional responsibilities', 'Make a positive contribution to the wider life and ethos of the College. communicate effectively with parents with regard to pupils’ achievements and well- being', 7, 1, NULL),
-(5, 'New Target', 'New target...', 7, 1, '22/03/2013'),
-(6, 'Fulfil wider professional responsibilities', 'Make a positive contribution to the wider life and ethos of the College. communicate effectively with parents with regard to pupilsâ€™ achievements and well- being', 7, 1, '04/03/2013'),
-(7, 'Fulfil wider professional responsibilities', 'Make a positive contribution to the wider life and ethos of the College. communicate effectively with parents with regard to pupilsÃ¢â‚¬â„¢ achievements and well- being', 7, 1, '04/03/2013');
+INSERT INTO `target` (`id`, `title`, `title_ext`, `description`, `status_id`, `employee_id`, `target_date`) VALUES
+(1, '1 Teaching and Learning', 'Assessment and Feedback', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html>\n<head>\n</head>\n<body>\n<div>&middot; Use a range of assessment methods which evaluate the learning in the classroom every 10 minutes.</div>\n<div>&nbsp;</div>\n<div>&middot; Provide effective written feedback for assignments within 4 days of submission.</div>\n<div>&nbsp;</div>\n<div>&middot; Use verbal praise and recognition in the classroom environment.</div>\n', 7, 1, '15/05/2013'),
+(2, '2 ACR/Departmental', 'Raise retention on course ABX5689DHJ from 89% to 95%.', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html>\n<head>\n</head>\n<body>\n<p>&middot; Liaise with Christine Bunting in ALS regarding the adjustments for dyslexia needed for resources.</p>\n<p>&middot; Ensure all lesson plans include the differentiated activities and assessments for students X and Y.</p>\n<p>&middot; Share the lesson plan in advance with their LSP.</p>\n</body>\n</html>', 7, 1, '19/04/2013'),
+(4, '3 Student focus', 'Raise retention on course ABX5689DHJ from 89% to 95%.', '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n<html>\n<head>\n</head>\n<body>\n<p>&middot; Feedback states students leave due to lack of enrichment activities and resources available on the VLE.</p>\n<p>&middot; Work with manager to identify good practice</p>\n<p>&middot; peer observe colleague with desired use of ILT</p>\n<p>&middot; Work with colleagues A and B to design 3 enrichment activities and trips which address the employability skills of teamwork, problem solving and communication.</p>\n</body>\n</html>', 7, 1, '18/07/2013');
 
 -- --------------------------------------------------------
 
@@ -239,7 +177,6 @@ INSERT INTO `target` (`id`, `title`, `description`, `status_id`, `employee_id`, 
 -- Table structure for table `target_status`
 --
 
-DROP TABLE IF EXISTS `target_status`;
 CREATE TABLE IF NOT EXISTS `target_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
@@ -260,11 +197,11 @@ INSERT INTO `target_status` (`id`, `title`, `sort_order`) VALUES
 --
 -- Stand-in structure for view `v_activity`
 --
-DROP VIEW IF EXISTS `v_activity`;
 CREATE TABLE IF NOT EXISTS `v_activity` (
 `id` int(11)
 ,`employee_id` int(11)
 ,`title` varchar(250)
+,`impact` varchar(250)
 ,`provider` varchar(250)
 ,`learning_outcomes` varchar(250)
 ,`planned_date` date
@@ -284,10 +221,10 @@ CREATE TABLE IF NOT EXISTS `v_activity` (
 --
 -- Stand-in structure for view `v_targets_with_status`
 --
-DROP VIEW IF EXISTS `v_targets_with_status`;
 CREATE TABLE IF NOT EXISTS `v_targets_with_status` (
 `id` int(11)
 ,`title` varchar(150)
+,`title_ext` varchar(150)
 ,`description` varchar(600)
 ,`status` varchar(50)
 ,`employee_id` int(20)
@@ -300,7 +237,7 @@ CREATE TABLE IF NOT EXISTS `v_targets_with_status` (
 --
 DROP TABLE IF EXISTS `v_activity`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_activity` AS (select `activity`.`id` AS `id`,`activity`.`employee_id` AS `employee_id`,`activity`.`title` AS `title`,`activity`.`provider` AS `provider`,`activity`.`learning_outcomes` AS `learning_outcomes`,`activity`.`planned_date` AS `planned_date`,`activity`.`cpd_type_id` AS `cpd_type_id`,`cpd_type`.`description` AS `cpd_type`,`activity`.`target_id` AS `target_id`,`target`.`title` AS `target`,`activity`.`priority_type_id` AS `priority_type_id`,`priority_type`.`description` AS `priority_type`,`activity`.`completed_date` AS `completed_date`,`activity`.`evaluation_url` AS `evaluation_url`,`activity`.`hours_of_cpd` AS `hours_of_cpd`,`activity`.`rating` AS `rating` from (((`activity` left join `cpd_type` on((`activity`.`cpd_type_id` = `cpd_type`.`id`))) left join `priority_type` on((`activity`.`priority_type_id` = `priority_type`.`id`))) left join `target` on((`activity`.`target_id` = `target`.`id`))));
+CREATE ALGORITHM=UNDEFINED DEFINER=`mycpd_admin`@`%` SQL SECURITY DEFINER VIEW `v_activity` AS (select `activity`.`id` AS `id`,`activity`.`employee_id` AS `employee_id`,`activity`.`title` AS `title`,`activity`.`impact` AS `impact`,`activity`.`provider` AS `provider`,`activity`.`learning_outcomes` AS `learning_outcomes`,`activity`.`planned_date` AS `planned_date`,`activity`.`cpd_type_id` AS `cpd_type_id`,`cpd_type`.`description` AS `cpd_type`,`activity`.`target_id` AS `target_id`,`target`.`title` AS `target`,`activity`.`priority_type_id` AS `priority_type_id`,`priority_type`.`description` AS `priority_type`,`activity`.`completed_date` AS `completed_date`,`activity`.`evaluation_url` AS `evaluation_url`,`activity`.`hours_of_cpd` AS `hours_of_cpd`,`activity`.`rating` AS `rating` from (((`activity` left join `cpd_type` on((`activity`.`cpd_type_id` = `cpd_type`.`id`))) left join `priority_type` on((`activity`.`priority_type_id` = `priority_type`.`id`))) left join `target` on((`activity`.`target_id` = `target`.`id`))));
 
 -- --------------------------------------------------------
 
@@ -309,7 +246,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `v_targets_with_status`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`mycpd_admin`@`localhost` SQL SECURITY DEFINER VIEW `v_targets_with_status` AS select `target`.`id` AS `id`,`target`.`title` AS `title`,`target`.`description` AS `description`,`target_status`.`title` AS `status`,`target`.`employee_id` AS `employee_id`,`target`.`target_date` AS `target_date` from (`target` join `target_status` on((`target`.`status_id` = `target_status`.`id`)));
+CREATE ALGORITHM=UNDEFINED DEFINER=`mycpd_admin`@`%` SQL SECURITY DEFINER VIEW `v_targets_with_status` AS select `target`.`id` AS `id`,`target`.`title` AS `title`,`target`.`title_ext` AS `title_ext`,`target`.`description` AS `description`,`target_status`.`title` AS `status`,`target`.`employee_id` AS `employee_id`,`target`.`target_date` AS `target_date` from (`target` join `target_status` on((`target`.`status_id` = `target_status`.`id`)));
 
 --
 -- Constraints for dumped tables
@@ -323,19 +260,6 @@ ALTER TABLE `activity`
   ADD CONSTRAINT `activity_ibfk_2` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
   ADD CONSTRAINT `Learning_plan_detail_ibfk_2` FOREIGN KEY (`target_id`) REFERENCES `target` (`id`),
   ADD CONSTRAINT `Learning_plan_detail_ibfk_3` FOREIGN KEY (`priority_type_id`) REFERENCES `priority_type` (`id`);
-
---
--- Constraints for table `employee_section`
---
-ALTER TABLE `employee_section`
-  ADD CONSTRAINT `employee_section_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
-  ADD CONSTRAINT `employee_section_ibfk_2` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`);
-
---
--- Constraints for table `section`
---
-ALTER TABLE `section`
-  ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`);
 
 --
 -- Constraints for table `target`
