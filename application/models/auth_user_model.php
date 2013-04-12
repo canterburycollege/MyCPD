9004dcf94d2868aee3dcc37cb7d882027f7ffc42
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-04-12 11:20:29 -0300

    remove test functions

--- application/models/auth_user_model.php
+++ application/models/auth_user_model.php
@@ -10,7 +10,7 @@ class Auth_user_model extends CI_Model {
     /**
      * Query db for authorised user details
      * 
-     * @return object
+     * @return object Contains details from employee table.
      */
     public function get_auth_user() {
         $logged_in_user = $this->get_logged_in_user();
@@ -21,9 +21,6 @@ class Auth_user_model extends CI_Model {
             show_error('cannot find user ('
                     . $ldap_username . ') in database');
         }
-        
-        ##$data = new stdClass();
-        ##$data->id = 1; // set for testing
 
         return $data;
     }
@@ -95,61 +92,7 @@ class Auth_user_model extends CI_Model {
             }
         }
     }
-
-    /**
-     * Force moodle login & get user details
-     * 
-     * Attributes include id, username(ldap), firstname, lastname, email
-     * 
-     * @return object Moodle user details (return NULL, if not logged in)
-     */
-    private function get_moodle_user() {
-
-        /**
-         * @todo make sure moodle path is correct!
-         */
-        // call moodle config file
-        require_once '../../config.php';
-        // force login
-        require_login();
-        $user_object = $DB->get_record('user', array('id' => $USER->id));
-
-        return $user_object;
-    }
-
-    /**
-     * For testing/demo
-     * 
-     * Insert dummy row with id=1, if doesn't already exist
-     */
-    public function test_auth_user() {
-
-        $affected_rows = 0;
-        $test_id = 1;
-        $tbl = 'mycpd.employee';
-        $row = array(
-            'id' => $test_id,
-            'display_name' => 'Treesa Green',
-            'moodle_user_id' => '99',
-            'mycpd_access_group' => 'test'
-        );
-
-        // check if row already exists and insert, if not
-        $query = $this->db->get_where($tbl, array('id' => $test_id), 1, 0);
-        if ($query->num_rows() == 0) {
-            $this->db->insert($tbl, $row);
-            $affected_rows = $this->db->affected_rows();
-        }
-        // echo msg if test row inserted (should really return msg!)
-        if ($affected_rows > 0) {
-            //echo "<p>Test msg : {$affected_rows} test row inserted into employee table</p>";
-        } else {
-            //echo '<p>Test msg : test employee already exists in employee table</p>';
-        }
-    }
-
 }
 
 /* End of file auth_user_model.php */
 /* Location: ./models/auth_user_model.php */
-
